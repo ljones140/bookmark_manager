5ffe3d6cd4c56082649bca079e04872e9826a1d7
lewis <ljones140@gmail.com>
2015-08-10 19:46:24 +0100

    database test and dev set up

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,10 +1,20 @@
 require 'sinatra/base'
-
+# require '../data_mapper_setup'
+# require_relative './models/link.rb'
 
 class BookmarkManager < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
     'Hello BookmarkManager!'
   end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require 'rubygems'
-require File.join(File.dirname(__FILE__), 'lib/bookmark_manager.rb')
+require File.join(File.dirname(__FILE__), 'data_mapper_setup.rb')
+require File.join(File.dirname(__FILE__), 'app/bookmark_manager.rb')
 
 run BookmarkManager
